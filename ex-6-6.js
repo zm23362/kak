fdb25caa0e40bdfcf47090d8247fb5e524060cf9
zm23362 <zm23362@josai.ac.jp>
2025-10-28 10:24:49 +0900

    Update ex-6-6.js

--- ex-6-6.js
+++ ex-6-6.js
@@ -1,1 +1,6 @@
-
+var who66 = 'world';
+function sayhello66() {alert('Hello,' + who66 + '!');}
+var whoo66 = '太郎';
+function taro66() {alert('Hello,' + whoo66 + '!');}
+var whooo66 = '花子';
+function hanako66() {alert('Hello,' + whooo66 + '!');}

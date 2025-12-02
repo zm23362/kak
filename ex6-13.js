a95471c227182228ef578bc1dac3aacc68603499
zm23362 <zm23362@josai.ac.jp>
2025-12-02 11:51:33 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -1,2 +1,2 @@
-function imgchange3() {document.getElementById('long').src ='./image/josai1.jpg';}
-function imgchange4() {document.getElementById('logo').src ='./image/josai.jpg';}
+function imgchange3() {document.getElementById('logo1').src ='./image/josai1.jpg';}
+function imgchange4() {document.getElementById('logo2').src ='./image/josai.jpg';}

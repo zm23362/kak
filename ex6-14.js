aa4734074207334aff8031ee1882002d311b12e1
zm23362 <zm23362@josai.ac.jp>
2025-12-08 14:53:27 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,3 @@
-function imgchange5() {document.getElementById('logo2').src ='./image/aka.jpg';}
-function imgchange6() {document.getElementById('logo2').src ='./image/aki.jpg';}
+function imgchange5() {document.getElementById('logo2').src ='./image/aki.jpg';}
+function imgchange6() {document.getElementById('logo2').src ='./image/aka.jpg';}
 

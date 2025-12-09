055d2122e933d227c5dc43030a1a61e273639569
zm23362 <zm23362@josai.ac.jp>
2025-12-09 10:31:04 +0900

    Create ex6-15.js

---
+++ ex6-15.js
@@ -0,0 +1,3 @@
+if (cnt<11){//現在より1文字多く切り出して表示 document.timer.moji.value=str.substr(0,cnt+1);}
+else{//全て表示されたら、空文字に戻す document.timer.moji.value ="";}
+function startfnc(){//関数hyoji()を1000ミリ秒間隔で呼び出す setlnterval("hyoji()",1000);}

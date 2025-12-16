d563882f5b6107b17510526377ec2e21ccf478cc
zm23362 <zm23362@josai.ac.jp>
2025-12-16 09:51:49 +0900

    Update ex6-15.js

--- ex6-15.js
+++ ex6-15.js
@@ -1,7 +1,7 @@
 function hyuji(){//表示する文字 Var str="1文字ずつ表示します。";
-
-
-
-if (cnt<11){//現在より1文字多く切り出して表示 document.timer.moji.value=str.substr(0,cnt+1);}
-else{//全て表示されたら、空文字に戻す document.timer.moji.value ="";}
+//テキストボックスの文字数
+ var cnt=document.timer.moji.value.length;
+//文字が全部表示されているか確認
+ if (cnt<11){//現在より1文字多く切り出して表示 document.timer.moji.value=str.substr(0,cnt+1);}
+else{//全て表示されたら、空文字に戻す document.timer.moji.value ="";}}
 function startfnc(){//関数hyoji()を1000ミリ秒間隔で呼び出す setlnterval("hyoji()",1000);}

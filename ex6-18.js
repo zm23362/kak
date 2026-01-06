422bba8c209674c469b34bdbf551777b7476d6aa
zm23362 <zm23362@josai.ac.jp>
2026-01-06 10:27:23 +0900

    Update ex6-18.js

--- ex6-18.js
+++ ex6-18.js
@@ -1,3 +1,3 @@
 function changeImage() { var image = document.getElementById('myImage');
-image.src = 'boo.gif'; //新しい画像に変更//3秒後に元の画像に戻す \mathbf{setTimeout(function(){
-image.src = 'boff.gif'; //元の画像に変更}, 3000);} //3000ミリ秒＝3秒
+image.src = 'boo.jpg'; //新しい画像に変更//3秒後に元の画像に戻す \mathbf{setTimeout(function(){
+image.src = 'boff.jpg'; //元の画像に変更}, 3000);} //3000ミリ秒＝3秒

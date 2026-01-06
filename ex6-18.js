c20729eca1f37b09ade6fa2265c4dff27060d570
zm23362 <zm23362@josai.ac.jp>
2026-01-06 10:00:50 +0900

    Update ex6-18.js

--- ex6-18.js
+++ ex6-18.js
@@ -1,2 +1,3 @@
 function changeImage() {//画像を一時的に別の画像に変更する
 document.getElementById('myImage').src = 'boff.gif';
+<form> <input type="button" value="Timer" onclick="setTimeout('mes()',3000)"> </form>

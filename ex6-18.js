b97cc43938eccfbc0e4ec5ecbe04f5e58cd2cd0a
zm23362 <zm23362@josai.ac.jp>
2026-01-25 18:44:04 +0900

    Update ex6-18.js

--- ex6-18.js
+++ ex6-18.js
@@ -1,4 +1,13 @@
-function changeImage() {const img = document.getElementById('myImage');
-img.src = 'boo.jpg'; // 切り替え後の画像パスに変更
-setTimeout(function() {img.src = 'boff.jpg'; // 3秒後に元の画像パスに戻す 
-                      }, 3000);}
+ function changeImage() {
+    const imgElement = document.getElementById('logo');
+    const originalSrc = './image/boff.jpg';
+    const newSrc = './image/boo.jpg'; // 切り替え後の画像パス
+
+    // 画像を切り替える
+    imgElement.src = newSrc;
+
+    // 3秒後に元の画像に戻す
+    setTimeout(() => {
+      imgElement.src = originalSrc;
+    }, 3000); // 3000ミリ秒 = 3秒
+  }
